a868418399bfb45e76a69373189a4c6a486d8b67
herbwood <herbwood27@gmail.com>
2020-09-10 16:34:55 +0900

    minor changes

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -1,8 +1,9 @@
 from imutils import paths
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from keras.utils import np_utils
 import cv2
-import numpy 
+import numpy as np
 from utils.utils import configInfo
 import os
 
@@ -10,12 +11,13 @@ import os
 class DataLoader:
     def __init__(self, config="../config.json"):
         self.config = configInfo(config)
+        print(self.config)
         self.data = []
         self.labels = []
 
         print("[INFO] loading images...")
         imagePaths = list(paths.list_images(self.config["dataset"]))
-
+        print(imagePaths)
 
         for imagePath in imagePaths:
             # 파일 이름에서 클래스 레이블을 추출하고 이미지를 로드한 다음, 32x32 크기 조정  
@@ -27,20 +29,11 @@ class DataLoader:
             self.data.append(image)
             self.labels.append(label)
 
-        self.data = imagePreprocess(self.data)
-        self.label = labelPreprocess(self.labels)
-        
-
-
-    def imagePreprocess(self, data):
-        return np.array(data, dtype="float") / 255.0
-    
-    def labelPreprocess(self, labels):
+        self.data = np.array(self.data, dtype="float") / 255.0
         le = LabelEncoder()
-        labels = le.fit_transform(labels)
-        labels = np_utils.to_categorical(labels, 2)
+        self.labels = le.fit_transform(self.labels)
+        self.labels = np_utils.to_categorical(self.labels, 2)
 
-        return labels
 
     def split(self, test_size=0.25):
         (trainX, testX, trainY, testY) = train_test_split(self.data, self.labels, test_size=test_size, random_state=42)
@@ -48,5 +41,6 @@ class DataLoader:
 
 if __name__ == "__main__":
     dl = DataLoader()
+    print(dl.data)
     trainX, testX, trainY, testY = dl.split()
     print(trainX)
