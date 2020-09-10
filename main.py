0c776531f7470eb9460d83cbfb6f4156f20b82a8
herbwood <herbwood27@gmail.com>
2020-09-10 18:26:33 +0900

    incorparated dataloader, model, trainer

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from data_loader.data_generator import DataLoader
+from model.livenessnet import LivenessNet
+from trainer.trainer import Trainer
 from utils.utils import configInfo
 
 def main():
-    dl = DataLoader(config="config.json")
-    trainX, testX, trainY, testY = dl.split()
-    print(trainX)
-    print(f'print {__name__}')
+    dataloader = DataLoader(config="config.json")
+    model = LivenessNet.build(width=32, height=32, depth=3, classes=2)
+    trainer = Trainer(dataloader, config="config.json")
+    trainer.train()
 
 if __name__ == "__main__":
     main()
