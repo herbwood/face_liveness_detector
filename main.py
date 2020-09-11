97b1cbeebf02a55ceb5568870b767905352b9459
herbwood <herbwood27@gmail.com>
2020-09-11 17:04:42 +0900

    deleted previous codes to use it as demo

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from data_loader.data_generator import DataLoader
 from model.livenessnet import LivenessNet
 from trainer.trainer import Trainer
-from utils.utils import configInfo
 
 def main():
-    dataloader = DataLoader(config="config.json")
-    model = LivenessNet.build(width=32, height=32, depth=3, classes=2)
-    trainer = Trainer(dataloader, config="config.json")
-    trainer.train()
+    print(__name__)
 
 if __name__ == "__main__":
     main()
