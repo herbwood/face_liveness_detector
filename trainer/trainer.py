9a0ed160112b4664d1f576960ec4b8bc67fe9590
herbwood <herbwood27@gmail.com>
2020-09-12 22:16:37 +0900

    changed code to tensorflow 2.3.0 version

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,47 +1,35 @@
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam
-from utils.utils import  configInfo
-from sklearn.metrics import classification_report
+import tensorflow as tf
+from data_loader.data_generator import DataLoader
+from utils.utils import configInfo, visualization
+from model.livenessnet import LivenessNet
 
 class Trainer:
-    def __init__(self, dataloader, model, config, INIT_LR=1e-4, BATCH_SIZE=8, EPOCHS=50):
+
+    def __init__(self, dataloader, model, config, batch_size=8, epochs=2):
 
         self.dataloader = dataloader
         self.model = model
         self.config = configInfo(config)
-        self.lr = INIT_LR
-        self.BS = BATCH_SIZE
-        self.EPOCHS = EPOCHS
-        self.aug = ImageDataGenerator(rotation_range=20, zoom_range=0.15,
-                                 width_shift_range=0.2, height_shift_range=0.2, shear_range=0.15,
-                                 horizontal_flip=True, fill_mode="nearest")
-        self.saved_model = self.config["saved_model"]
-        self.saved_le = self.config["saved_le"]
-        self.leclasses = dataloader.leclasses
+        self.batch_size = batch_size
+        self.epochs = epochs
 
-
+        self.train_generator, self.validation_generator = self.dataloader.data_generator()
 
     def train(self):
 
-        trainX, testX, trainY, testY = self.dataloader.split()
-        opt = Adam(lr=self.lr, decay=self.lr / self.EPOCHS)
-        # model = LivenessNet.build(width=32, height=32, depth=3, classes=2)
-        self.model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+        self.model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+        print(self.model.summary())
+        history = self.model.fit(self.train_generator, epochs=self.epochs,
+                                 validation_data=self.validation_generator, verbose=1, steps_per_epoch=len(self.train_generator) / self.epochs)
+        #
 
-        # 딥러닝 학습
-        print("[INFO] training network for {} epochs...".format(self.EPOCHS))
-        H = self.model.fit(self.aug.flow(trainX, trainY, batch_size=self.BS),
-                                     validation_data=(testX, testY), steps_per_epoch=len(trainX) // self.BS,
-                                     epochs=self.EPOCHS)
+        self.model.save(self.config["saved_model"])
 
-        print(f"[INFO] serializing network to '{self.saved_model}'...")
-        self.model.save(self.saved_model)
+        return history
 
-        print("[INFO] evaluating network...")
-        predictions = self.model.predict(testX, batch_size=self.BS)
-        print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=self.dataloader.leclasses))
-
-        return H
-
-
-
+if __name__ == "__main__":
+    dataloader = DataLoader(config="../config.json")
+    model = LivenessNet().build()
+    trainer = Trainer(dataloader, model, config="../config.json")
+    history = trainer.train()
+    visualization(history)
