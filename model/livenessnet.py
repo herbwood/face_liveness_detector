18bfd9a901f65e940dc758f07b968988de7e5df7
herbwood <herbwood27@gmail.com>
2020-09-12 22:14:56 +0900

    changed code to tensorflow 2.3.0 version

--- model/livenessnet.py
+++ model/livenessnet.py
@@ -1,61 +1,39 @@
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dropout
-from keras.layers.core import Dense
-from keras import backend as K
-
+import tensorflow as tf
 
 class LivenessNet:
+  def __init__(self, width=32, height=32, depth=3, classes=1):
+    self.width = width
+    self.height = height
+    self.depth = depth
+    self.classes = classes
 
-    def __init__(self, width=32, height=32, depth=3, classes=2):
-        self.width = width
-        self.height = height
-        self.depth = depth
-        self.classes = classes
+  def build(self):
+    input_shape = (self.height, self.width, self.depth)
 
-    def build(self):
-        model = Sequential()
-        inputShape = (self.height, self.width, self.depth)
-        chanDim = -1
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same", input_shape=input_shape))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same", input_shape=input_shape))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(rate=0.25))
 
-        if K.image_data_format() == "channel_first":
-            inputShape = (self.depth, self.height, self.width)
-            chanDim = 1
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation="relu", padding="same", input_shape=input_shape))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation="relu", padding="same", input_shape=input_shape))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(rate=0.25))
 
-        model.add(Conv2D(16, (3, 3), padding="same", input_shape=inputShape))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
-        model.add(Conv2D(16, (3, 3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(64, activation="relu"))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dropout(rate=0.5))
 
-        model.add(Conv2D(32, (3, 3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
-        model.add(Conv2D(32, (3, 3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
+    model.add(tf.keras.layers.Dense(self.classes, activation="sigmoid"))
 
-        model.add(Flatten())
-        model.add(Dense(64))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization())
-        model.add(Dropout(0.5))
-
-        model.add(Dense(self.classes))
-        model.add(Activation("softmax"))
-
-        return model
-
+    return model
 
 if __name__ == "__main__":
-    model = LivenessNet()
-    print(model)
+    model = LivenessNet().build()
+    print(model.summary())
