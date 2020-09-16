3af3ebe1cf9cb0aed1d2302f9be9f7bb1fbb3a27
herbwood <herbwood27@gmail.com>
2020-09-16 15:01:10 +0900

    added detailed optimizer and steps per epoch parameters

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -5,23 +5,35 @@ from model.livenessnet import LivenessNet
 
 class Trainer:
 
-    def __init__(self, dataloader, model, config, batch_size=8, epochs=2):
+    def __init__(self, dataloader, model, config, batch_size=32, epochs=50):
 
         self.dataloader = dataloader
         self.model = model
         self.config = configInfo(config)
         self.batch_size = batch_size
         self.epochs = epochs
+        self.INIT_LR = (1e-5)/4
+        self.optimizer = tf.keras.optimizers.Adam(lr=self.INIT_LR, decay=self.INIT_LR/self.epochs)
 
         self.train_generator, self.validation_generator = self.dataloader.data_generator()
 
+        self.step_size_train = self.train_generator.n // self.train_generator.batch_size
+        self.step_size_validation = self.validation_generator.samples // self.validation_generator.batch_size
+
+
     def train(self):
 
-        self.model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+        self.model.compile(optimizer=self.optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
         print(self.model.summary())
+
+        early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=5)
+
         history = self.model.fit(self.train_generator, epochs=self.epochs,
-                                 validation_data=self.validation_generator, verbose=1, steps_per_epoch=len(self.train_generator) / self.epochs)
-        #
+                                 validation_data=self.validation_generator, verbose=1,
+                                 steps_per_epoch=self.step_size_train,
+                                 validation_steps=self.step_size_validation,
+                                 callbacks=[early_stopping])
+        #, steps_per_epoch=len(self.train_generator) / self.epochs
 
         self.model.save(self.config["saved_model"])
 
