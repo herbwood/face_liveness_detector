df59feaa2598121df60152a383959392e249e92e
herbwood <herbwood27@gmail.com>
2020-09-18 20:05:59 +0900

    added trialnet

--- train.py
+++ train.py
@@ -2,10 +2,11 @@ from data_loader.data_loader import DataLoader
 from utils.utils import configInfo, visualization
 from model.livenessnet import LivenessNet
 from trainer.trainer import Trainer
+from model.trialnet import TrialNet
 
 def main():
     dataloader = DataLoader(config="config.json")
-    model = LivenessNet(config="config.json").build()
+    model = TrialNet(config="config.json").build()
     trainer = Trainer(dataloader, model, config="config.json")
     history = trainer.train()
     visualization(history)
