9f035950e8acdb9d9fb9c84a165162b40b86afbc
herbwood <herbwood27@gmail.com>
2020-09-18 20:06:38 +0900

    added loss visualization

--- utils/utils.py
+++ utils/utils.py
@@ -10,13 +10,18 @@ def visualization(history):
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
 
-    # loss = history.history['loss']
-    # val_loss = history.history['val_loss']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
 
     epochs = range(len(acc))
 
-    plt.plot(epochs, acc, 'r', label='Training accuracy')
-    plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    # plt.plot(epochs, acc, 'r', label='Training accuracy')
+    # plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    # plt.title('Training and validation accuracy')
+    # plt.legend(loc=0)
+
+    plt.plot(epochs, loss, 'r', label='Training Loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation Loss')
     plt.title('Training and validation accuracy')
     plt.legend(loc=0)
     # plt.figure()
