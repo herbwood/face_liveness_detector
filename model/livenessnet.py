e07cd8d5925d7bfd59c710a3d02c756eb479887e
herbwood <herbwood27@gmail.com>
2020-09-19 19:29:37 +0900

    changed parameter

--- model/livenessnet.py
+++ model/livenessnet.py
@@ -3,12 +3,11 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 from utils.utils import configInfo
 
 class LivenessNet:
-  def __init__(self, config, width=128, height=128, depth=3, classes=2):
+  def __init__(self, config):
     self.config = configInfo(config)
-    self.width = width
-    self.height = height
-    self.depth = depth
-    self.classes = classes
+    self.hyperparameters = self.config["hyperparameters"]
+    self.width, self.height, self.depth = self.hyperparameters["size"]
+    self.classes = self.config["le"]["num_classes"]
 
   def build(self):
     input_shape = (self.height, self.width, self.depth)
@@ -43,3 +42,7 @@ class LivenessNet:
     model.add(tf.keras.layers.Dense(self.classes, activation="softmax"))
 
     return model
+
+if __name__ == "__main__":
+  model = LivenessNet("../config.json").build()
+  print(model.summary())
