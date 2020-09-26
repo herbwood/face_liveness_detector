dad192c3e9372881a5817d60ec796617dfc1aec6
herbwood <herbwood27@gmail.com>
2020-09-26 13:34:54 +0900

    load images by directory

--- face_verification/face_verification.py
+++ face_verification/face_verification.py
@@ -16,24 +16,24 @@ class FaceVerification:
         process_this_frame = True
 
         image_path = self.config["face_verification"]
+        # image_path = "../image"
+        for dir in os.listdir(image_path):
+            if dir == "frame" or dir == ".gitkeep":
+                continue
+            for image in os.listdir(os.path.join(image_path, dir)):
+                name = os.path.splitext(os.path.basename(os.path.join(image_path, dir, image)))[0]
 
-        for image in os.listdir(image_path):
-            if image == "frame":
-                continue
-            name = os.path.splitext(os.path.basename(os.path.join(image_path, image)))[0]
-            if name == ".gitkeep":
-                continue
-            loaded_image = fr.load_image_file(os.path.join(image_path, image))
-            loaded_face_encodings = fr.face_encodings(loaded_image)[0]
-            known_face_encodings.append(loaded_face_encodings)
-            known_face_names.append(name)
+                loaded_image = fr.load_image_file(os.path.join(image_path, dir, image))
+                loaded_face_encodings = fr.face_encodings(loaded_image)[0]
+                known_face_encodings.append(loaded_face_encodings)
+                known_face_names.append(dir)
 
         return known_face_encodings, known_face_names, face_locations, face_encodings, face_names, process_this_frame
 
 
 if __name__ == "__main__":
-    fv = FaceVerification("../config.json")
+    fv = FaceVerification("../config/config.json")
     a, b, d, e, f, g = fv.face_information()
-    print(a, b)
+    print(b)
 
 
