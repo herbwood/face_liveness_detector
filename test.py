811bd13cff5ecdfed69e3a305b00966d468146bb
herbwood <herbwood27@gmail.com>
2020-09-27 19:43:37 +0900

    added logtest

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 def test(dataloader, config="config/config.json"):
     config = configInfo(config)
-    model = tf.keras.models.load_model(config["best_saved_model"])
+    model = tf.keras.models.load_model(config["trial_saved_model"])
 
     _, validation_generator = dataloader.data_generator()
     batch_size = dataloader.batch_size
@@ -18,6 +18,33 @@ def test(dataloader, config="config/config.json"):
     print('Classification Report')
     print(classification_report(validation_generator.classes, y_pred))
 
+def logTest(config):
+    with open(config) as f:
+        logs = f.readlines()
+
+    total = len(logs)
+    real_known_cnt, real_unknown_cnt,  fake_known_cnt, fake_unknown_cnt = 0, 0, 0, 0
+
+    for log in logs:
+        log = log.strip()
+        name, label, prob, width, height, channel = log.split()[2:]
+
+        if name != "Unknown" and label == "real":
+            real_known_cnt += 1
+        elif name == "Unknown" and label == "real":
+            real_unknown_cnt += 1
+        elif name != "Unknown" and label == "fake":
+            fake_known_cnt += 1
+        else:
+            fake_unknown_cnt += 1
+
+    print(f"          |    real   |     fake    |   total     |")
+    print(f"| known   | {real_known_cnt}({round(real_known_cnt / total, 2)}) |   {fake_known_cnt}({round(fake_known_cnt/total, 2)})  | {real_known_cnt + fake_known_cnt}({round((real_known_cnt + fake_known_cnt)/total, 2)})   |")
+    print(f"| unknown | {real_unknown_cnt}({round(real_unknown_cnt/total, 2)})   |   {fake_unknown_cnt}({round(fake_unknown_cnt/total, 2)})   |  {real_unknown_cnt + fake_unknown_cnt}({round((real_unknown_cnt + fake_unknown_cnt)/total, 2)})   |")
+    print(f"| total   | {real_known_cnt + real_unknown_cnt}({round((real_known_cnt + real_unknown_cnt)/total, 2)})  |    {fake_known_cnt + fake_unknown_cnt}({round((fake_known_cnt + fake_unknown_cnt)/total, 2)})  | {total}(1)      |")
+
+
 if __name__ == "__main__":
     dataloader = DataLoader(config="config/config.json")
-    test(dataloader)
+    # test(dataloader)
+    logTest("config/logs_19_06_31.log")
