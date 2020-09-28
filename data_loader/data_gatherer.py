20d88f0b34d37d244b27a620254e94d41104309d
herbwood <herbwood27@gmail.com>
2020-09-28 11:26:13 +0900

    added gather sample code

--- data_loader/data_gatherer.py
+++ data_loader/data_gatherer.py
@@ -1,63 +1,54 @@
-import os
 import shutil
+import face_recognition as fr
+import cv2
+from utils.utils import configInfo
+import os
 
-# 400명 인당 100
-# sl : 악세서리 착용 여부(s1, 2)
-# ll : 조도(l1, 2, 3, 8, 9)
-# el : 표정(e1, 2)
-# cl : 포즈 방향(c4, 7, 10, 19, 20)장
+def gather_example(config, saved_video_path, image_save_path):
+    config = configInfo(config)
 
-# def sampleTrainDataset(config, basepath='temp', targetpath='whole'):
-#     # sl, ll, el, cl을 조합하여 우리가 추출할 이미지 데이터 path를 지정해줌
-#     config = configInfo(config)
-#     sl, ll, el, cl = config["data_gatherer"]
-#
-#     # zip 파일을 풀면 그냥 생짜로 S001~S006 디렉터리를 풀어버려서 임시로 보관할 디렉터리가 필요해서
-#     # temp 디렉터리 안에 압축을 해제함
-#     # whole은 추출한 이미지 데이터만 저장하는 디렉터리
-#     # directory 변수에 zipfile이 저장된 디렉터리로 지정해주고
-#     # 데이터셋 zipfile말고 다른 zipfile은 없개끔!!!!!!!!!!!!
-#     # 저처럼 그냥 zipfile이 저장된 디렉터리에서 실행시킬거면 directory 안바꾸셔도 되요
-#     directory = './'
-#     os.chdir(directory)
-#
-#     # temp 디렉터리 만들어주기
-#     if basepath not in os.listdir():
-#         os.mkdir(basepath)
-#
-#     # whole 디렉터리 만들어주기
-#     if targetpath not in os.listdir():
-#         os.mkdir(targetpath)
-#
-#     # 디렉터리 내에 있는 모든 zip 파일을
-#     for zf in glob.glob('*.zip'):
-#         pathlist = []
-#         zipname = zf.split('.')[0]  # zip 파일 이름
-#
-#         # zip 파일을 temp 디렉터리에 압축 해제해줌
-#         zf = zipfile.ZipFile(zf)
-#         zf.extractall(os.path.join(basepath))
-#
-#         # (추출한 이미지 파일의 경로, 새롭게 지정할 이미지 파일의 이름) 형식으로 pathlist 변수에 저장함
-#         for s in sl:
-#             for l in ll:
-#                 for e in el:
-#                     for c in cl:
-#                         pathlist.append((os.path.join(s, l, e, c), '_'.join([zipname, s, l, e, c])))
-#
-#         # temp 디렉터리에 저장한 추출한 이미지 파일을 whole 디렉터리에 옮겨줌
-#         for path, pname in pathlist:
-#             shutil.move(os.path.join(basepath, path), os.path.join(targetpath, pname))
-#             print(path, pname)
-#
-#         # temp 디렉터리에 있는 쓰지 않는 디렉터리 및 파일 제거
-#         for dir in os.listdir(basepath):
-#             dpath = os.path.join(basepath, dir)
-#             if os.path.isdir(dpath):
-#                 shutil.rmtree(dpath)
-#
-#     # zip 파일을 전부 다 압축해제하면 temp 파일도 지워주기
-#     shutil.rmtree(basepath)
+    video_capture = cv2.VideoCapture(saved_video_path)
+    dirname = os.path.basename(saved_video_path).split(".")[0]
+
+    if dirname not in os.listdir(image_save_path):
+        os.mkdir(f"{image_save_path}/{dirname}")
+
+    i = 0
+    process_this_frame = True
+
+    while True:
+        ret, frame = video_capture.read()
+        if ret == False:
+            break
+
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]  # BGR을 RGB로 변환
+
+        if process_this_frame:
+            face_locations = fr.face_locations(
+                rgb_small_frame)  # Returns an array of bounding boxes of human faces in a image
+            face_encodings = fr.face_encodings(rgb_small_frame,
+                                               face_locations)  # Given an image, return the 128-dimension face encoding for each face in the image.
+
+        for (top, right, bottom, left) in face_locations:
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            face = frame[top:bottom, left:right]
+            if i % 10 == 0:
+                cv2.imwrite(f"{image_save_path}/{dirname}/{i}.jpg", face)
+                print(f"{dirname}_{i}.jpg saved")
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        else:
+            i += 1
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
 
 def extract_image_name(basepath, file):
     with open(file) as f:
@@ -73,3 +64,6 @@ def copy_images_to_dir(imagepathlist, target_dir):
     for imagepath in imagepathlist:
         filename = os.path.basename(imagepath)
         shutil.copy(imagepath, os.path.join(target_dir, filename))
+
+if __name__ == "__main__":
+    gather_example("../config/config.json", "../video/dami.mp4", "../image")
